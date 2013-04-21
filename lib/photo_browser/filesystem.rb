4e8e33832770bd40a490825095f4f6203593a97b
Kale Worsley <kale@catalyst.net.nz>
2013-04-21 15:26:59 +1200

    Fix exception names.

--- lib/photo_browser/filesystem.rb
+++ lib/photo_browser/filesystem.rb
@@ -8,29 +8,29 @@ module PhotoBrowser
 
 
     def self.months(year)
-      raise StoreInvalidYearError, "#{year} is invalid" unless File.directory? "#{BASE_PATH}/#{year}"
+      raise InvalidYearError, "#{year} is invalid" unless File.directory? "#{BASE_PATH}/#{year}"
         
       Dir["#{BASE_PATH}/#{year}/*"].select {|f| File.directory?(f) }.map {|f| File.basename f }
     end
 
     def self.photos(year, month)
-      raise StoreInvalidYearError, "#{year} is invalid" unless File.directory? "#{BASE_PATH}/#{year}"
-      raise StoreInvalidMonthError, "#{month} is invalid" unless File.directory? "#{BASE_PATH}/#{year}/#{month}"
+      raise InvalidYearError, "#{year} is invalid" unless File.directory? "#{BASE_PATH}/#{year}"
+      raise InvalidMonthError, "#{month} is invalid" unless File.directory? "#{BASE_PATH}/#{year}/#{month}"
 
       Dir["#{BASE_PATH}/#{year}/#{month}/*"].select {|f| File.file? f }.map {|f| File.basename f }
     end
 
     def self.photo(year, month, photo)
-      raise StoreInvalidYearError, "#{year} is invalid" unless File.directory? "#{BASE_PATH}/#{year}"
-      raise StoreInvalidMonthError, "#{month} is invalid" unless File.directory? "#{BASE_PATH}/#{year}/#{month}"
-      raise StoreInvalidPhotoError, "#{photo} is invalid" unless File.file? "#{BASE_PATH}/#{year}/#{month}/#{photo}"
+      raise InvalidYearError, "#{year} is invalid" unless File.directory? "#{BASE_PATH}/#{year}"
+      raise InvalidMonthError, "#{month} is invalid" unless File.directory? "#{BASE_PATH}/#{year}/#{month}"
+      raise InvalidPhotoError, "#{photo} is invalid" unless File.file? "#{BASE_PATH}/#{year}/#{month}/#{photo}"
 
       photo
     end
 
   end
 
-  class StoreInvalidYearError < Exception ; end
-  class StoreInvalidMonthError < Exception ; end
-  class StoreInvalidPhotoError < Exception ; end
+  class InvalidYearError < Exception ; end
+  class InvalidMonthError < Exception ; end
+  class InvalidPhotoError < Exception ; end
 end
