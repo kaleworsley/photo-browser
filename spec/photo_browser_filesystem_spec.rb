96cda6e905d1f23a0ca860f8278bc0151493094e
Kale Worsley <kale@worsley.co.nz>
2013-04-21 16:23:05 +1200

    Ignore array order in specs.

--- spec/photo_browser_filesystem_spec.rb
+++ spec/photo_browser_filesystem_spec.rb
@@ -4,18 +4,18 @@ PhotoBrowser::Filesystem::BASE_PATH = File.expand_path('spec/sandbox')
 
 describe PhotoBrowser::Filesystem do
   describe "::years" do
-    it { expect(PhotoBrowser::Filesystem.years).to eq ['2013'] }
+    it { PhotoBrowser::Filesystem.years.should =~ ['2013'] }
   end
 
   describe "::months" do
-    it { expect(PhotoBrowser::Filesystem.months('2013')).to eq ['Feb','Jan'] }
+    it { PhotoBrowser::Filesystem.months('2013').should =~ ['Feb','Jan'] }
     it { expect{PhotoBrowser::Filesystem.months('2012')}.to raise_error(PhotoBrowser::InvalidYearError) }
     it { expect{PhotoBrowser::Filesystem.months('invalid')}.to raise_error(PhotoBrowser::InvalidYearError) }
   end
 
   describe "::photos" do
-    it { expect(PhotoBrowser::Filesystem.photos('2013', 'Jan')).to eq ['photo1.jpg', 'photo2.jpg'] }
-    it { expect(PhotoBrowser::Filesystem.photos('2013', 'Feb')).to eq [] }
+    it { PhotoBrowser::Filesystem.photos('2013', 'Jan').should =~ ['photo1.jpg', 'photo2.jpg'] }
+    it { PhotoBrowser::Filesystem.photos('2013', 'Feb').should =~ [] }
     it { expect{PhotoBrowser::Filesystem.photos('2012', 'Jan')}.to raise_error(PhotoBrowser::InvalidYearError) }
     it { expect{PhotoBrowser::Filesystem.photos('2013', 'Mar')}.to raise_error(PhotoBrowser::InvalidMonthError) }
     it { expect{PhotoBrowser::Filesystem.photos('2013', 'invalid')}.to raise_error(PhotoBrowser::InvalidMonthError) }
